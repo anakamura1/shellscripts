35d24e75070d343721b4be854fdf1d3d43f805d1
Akira Nakamura <akiranakamura123@gmail.com>
2026-08-12 11:33:07 -0400

    Modified bash script

--- rmfalcon-sensor.sh
+++ rmfalcon-sensor.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 #Verify the falcon-sensor package is there
-dpkg -l | grep falcon-sensor
-if [ $? -gt 0 ]; then
+if [[ $EUID -ne 0 ]]; then
+echo "You must run this script as root/sudo"
+exit 1
+fi
+
+if ! dpkg -l falcon-sensor &>/dev/null; then
     echo "Falcon-Sensor is not installed. Move on from this machine."
     exit 0
 else
-    sudo apt-get purge falcon-sensor -y
-    dpkg -l | grep falcon-sensor
-    if [ $? -gt 0 ]; then
+    apt-get purge falcon-sensor -y
+    if ! dpkg -l falcon-sensor &>/dev/null; then
         echo "Falcone sensor is removed"
-        exit 0
+    else
+        echo "Falcon sensor is still present. Removal failed."
+        exit 1
     fi
 fi
